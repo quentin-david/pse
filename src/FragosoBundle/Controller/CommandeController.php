f6fa6fa2cf3304860161bf0f2b11dc5714db0682
David <quentin.david@live.fr>
2017-06-22 11:11:29 +0200

    Ajout Reglement

--- src/FragosoBundle/Controller/CommandeController.php
+++ src/FragosoBundle/Controller/CommandeController.php
@@ -5,7 +5,9 @@ namespace FragosoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use FragosoBundle\Entity\Commande;
+use FragosoBundle\Entity\Reglement;
 use FragosoBundle\Form\Type\CommandeType;
+use FragosoBundle\Form\Type\ReglementType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class CommandeController extends Controller
@@ -84,4 +86,57 @@ class CommandeController extends Controller
                             ));
     }
     
+    
+    /**
+     * Ajout d'un paiement à une commande
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function payerCommandeAction(Request $request, $client_num, $commande_num)
+    {
+        //Entity Manager
+        $em = $this->getDoctrine()->getManager();
+        
+        $client = $em->getRepository('FragosoBundle:Client')->find($client_num);
+        $commande = $em->getRepository('FragosoBundle:Commande')->find($commande_num);
+        
+        //Creation d'un objet de type Reglement
+        $reglement = new Reglement;
+        
+        // Creation du formulaire générique de création d'un reglement
+		$formulaire = $this->createForm(ReglementType::class, $reglement);
+		// On met le champ remise à la valeur par défaut du client
+        //$formulaire->getData()->setRemise($client->getRemise());
+        //$formulaire->setData($formulaire->getData());
+        
+        
+        //Enregistrement en base du formulaire
+        if($request->isMethod('POST')){
+            $formulaire->handleRequest($request);
+            if($formulaire->isValid()){
+                $reglement->setCommande($commande);
+                
+                if ($formulaire["montant"]->getData() > $commande->getResteAPayer()){	
+					$this->get('session')->getFlashBag()->add('warning','Paiement trop elevé'.$formulaire["montant"]->getData());
+                }else{
+					$this->get('session')->getFlashBag()->add('info','Paiement OK');
+					if ($formulaire["montant"]->getData() == $commande->getResteAPayer()){
+						$commande->setEtat('terminee');
+						$this->get('session')->getFlashBag()->add('info','La commande est entièrement payée');
+					}
+					$em->persist($reglement);
+					$em->persist($commande);
+					$em->flush();
+				}
+                
+				// Redirection vers la page catalogue
+                return $this->redirectToRoute('afficher_client', array('client_num' => $client->getId()));
+            }
+        }        
+        
+        return $this->render('FragosoBundle:Commande:reglement_edition.html.twig', array(
+                                    'formulaire' => $formulaire->createView(),
+                                    'client' => $client,
+                                    'commande' => $commande
+                            ));
+    }
 }
---
+++ src/FragosoBundle/Entity/Reglement.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace FragosoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Reglement
+ *
+ * @ORM\Table(name="reglement")
+ * @ORM\Entity(repositoryClass="FragosoBundle\Repository\ReglementRepository")
+ */
+class Reglement
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="montant", type="float")
+     */
+    private $montant;
+
+	/**
+     * @ORM\ManyToOne(targetEntity="FragosoBundle\Entity\Commande", inversedBy="reglements")
+     */
+    private $commande;
+    
+    
+    public function __construct()
+    {
+        // Par défaut, la date de la commande est la date d'aujourd'hui
+        $this->date = new \Datetime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Reglement
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set montant
+     *
+     * @param float $montant
+     *
+     * @return Reglement
+     */
+    public function setMontant($montant)
+    {
+        $this->montant = $montant;
+
+        return $this;
+    }
+
+    /**
+     * Get montant
+     *
+     * @return float
+     */
+    public function getMontant()
+    {
+        return $this->montant;
+    }
+
+    /**
+     * Set commande
+     *
+     * @param \FragosoBundle\Entity\Commande $commande
+     *
+     * @return Reglement
+     */
+    public function setCommande(\FragosoBundle\Entity\Commande $commande = null)
+    {
+        $this->commande = $commande;
+
+        return $this;
+    }
+
+    /**
+     * Get commande
+     *
+     * @return \FragosoBundle\Entity\Commande
+     */
+    public function getCommande()
+    {
+        return $this->commande;
+    }
+}
