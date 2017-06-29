6b7b66f6d5be5e45e6eb01f9baaf43332441a8b1
David <quentin.david@live.fr>
2017-06-29 16:46:08 +0200

    recherche des client avec List.js en cours

--- src/FragosoBundle/Controller/HomeController.php
+++ src/FragosoBundle/Controller/HomeController.php
@@ -10,11 +10,18 @@ class HomeController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $liste_commandes_encours = $em->getRepository('FragosoBundle:Commande')->findBy(array('etat' => 'encours'));
-        
+        $liste_clients_debiteurs = array();
+        foreach($em->getRepository('FragosoBundle:Client')->findAll() as $client)
+		{
+			if ($client->getSolde() > 0){
+				array_push($liste_clients_debiteurs, $client);
+			}
+		}
         
         
         return $this->render('FragosoBundle:Home:index.html.twig', array(
                                     'liste_commandes_encours' => $liste_commandes_encours,
+                                    'liste_clients_debiteurs' => $liste_clients_debiteurs,
                             ));
     }
 }
